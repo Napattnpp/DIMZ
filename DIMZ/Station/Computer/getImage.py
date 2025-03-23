01ea09265c6131a4291c30c1b3b84fff72099d55
Napattnpp <nnapat156@gmail.com>
2025-03-23 17:03:38 +0700

    Update getImage.py (Station)

--- DIMZ/Station/Computer/getImage.py
+++ DIMZ/Station/Computer/getImage.py
@@ -9,12 +9,25 @@ def save_image(port, baud_rate, output_path):
   # Open the serial port
   ser = serial.Serial(port, baud_rate, timeout=12)
 
-  # Open the output file
-  with open(output_path, 'wb') as img_file:
-    while True:
-      while chunk := ser.read(32):
+  open_file_status = False
+
+  while True:
+    # Read the first 32 bytes from the serial port
+    while chunk := ser.read(32):
+      # Check if the file is not open
+      if not open_file_status:
+        img_file = open(output_path, 'wb')
+        open_file_status = True
+
+        # Print the chunk
         print(chunk)
         img_file.write(chunk)
-      print("[Waiting for New Data...]")
+
+    if open_file_status:
+      img_file.close()
+      open_file_status = False
+      print("[Image Received]")
+
+    print("[Waiting for New Data...]")
 
 save_image('/dev/tty.usbmodem1101', 115200, 'received_image.jpg')
