4e7153fe10dbb20e5c2bb75fdd6ff0cb452d7113
Napattnpp <nnapat156@gmail.com>
2025-03-23 17:11:20 +0700

    Update new image path (cv2_image_result)

--- DIMZ/Transmitter/RPi/Send-Result/sendImageResult.py
+++ DIMZ/Transmitter/RPi/Send-Result/sendImageResult.py
@@ -7,5 +7,5 @@ config.read('pathConfig.ini')
 
 si = SI()
 # si.loadImage(image_path=config['paths']['ai_image_result'])
-si.getImage(save_image_path=config['paths']['ai_image_result'])
+si.getImage(save_image_path=config['paths']['cv2_image_result'])
 si.send()
