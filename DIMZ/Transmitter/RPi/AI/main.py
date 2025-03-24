f52f7aeb9db85aa2f87e8b44af2135f5d2827e48
Napattnpp <nnapat156@gmail.com>
2025-03-24 16:46:54 +0700

    Update AI/main.py

--- DIMZ/Transmitter/RPi/AI/main.py
+++ DIMZ/Transmitter/RPi/AI/main.py
@@ -35,39 +35,51 @@ def handle_exit(signal_received, frame):
 signal.signal(signal.SIGINT, handle_exit)   # Handle Ctrl+C (KeyboardInterrupt)
 signal.signal(signal.SIGTERM, handle_exit)  # Handle kill command
 
-def main():
+def main(save_image=True, save_text=True):
+    open_file_state = False
+
     try:
         # Process results and save to a text file (overwrite previous)
         while (1):
             # Start detection from webcam and MUST use stream=True for real-time processing
-            results = ncnn_model.predict(source=0, stream=True, conf=0.83)
+            results = ncnn_model.predict(source=0, stream=True, conf=0.78)
 
             for result in results:
                 # Check if any detection is made
                 if result.boxes is not None and len(result.boxes) > 0:
                     print("Detection Found!")
-                    detection_made = True
 
-                    # Process and save detected classes
-                    with open(ai_text_result_path, 'w') as file:
-                        for box in result.boxes:
+                    # Loop through each detected box
+                    for box in result.boxes:
+
+                        # If save_image is True, save the detected image
+                        if save_image:
                             # Get bounding box coordinates (x1, y1, x2, y2)
                             x1, y1, x2, y2 = map(int, box.xyxy[0])
                             # Save image result
                             cv2.imwrite(ai_image_result_path, result.orig_img[y1:y2, x1:x2])
                             print(f"Saved: {ai_image_result_path}")
 
-                            # Save text result
+                        # If save_text is True, save the detected text
+                        if save_text:
                             # box.cls() -> Return the class values of the boxes.
                             class_id = int(box.cls[0])
                             detected_label = class_mapping.get(class_id, f"unknown_{class_id}")
+
+                            # If the file is not open, open it
+                            if open_file_state == False:
+                                file = open(ai_text_result_path, 'w')
+                                open_file_state = True
+
                             file.write(detected_label)
                             print(f"Results saved to {ai_text_result_path}")
-                    
+
                     # Exit after saving the first detection
+                    file.close()
                     sys.exit(0)
     except Exception as e:
             print(f"Error: {e}")
+            file.close()
 
 try:
     main()
